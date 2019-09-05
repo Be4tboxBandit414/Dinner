08e201e33255b2c3959bcee34038cdbd36e8f216
Stanley <snguyen414@gmail.com>
2019-09-05 17:13:11 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-$(function(){
-
+$(function() {
   // var dinner =  ['Spaghetti', 'Beef and Broccoli', 'Steak', 'Salad', 'Breakfast for Dinner', 'Tacos']
-    var dinner = [
-      { "id": 1, "name": "Spaghetti" },
-      { "id": 2, "name": "Beef and Broccoli" },
-      { "id": 3, "name": "Sushi" },
-      { "id": 4, "name": "Salad" },
-      { "id": 5, "name": "Breakfast for Dinner" },
-      { "id": 6, "name": "Tacos" },
-      { "id": 7, "name": "Chicken" },
-      { "id": 8, "name": "Stanley's Choice =)" },
-      { "id": 9, "name": `Josh's Choice T____T` }
-    ]
+  var dinner = [
+    { id: 1, name: "Highest Team Point Total" },
+    { id: 2, name: "Highest Bench Point Total" },
+    { id: 3, name: "Highest Special Teams Point Total" },
+    { id: 4, name: "Highest RBs Point Total (Starting Lineup)" },
+    { id: 5, name: "Highest WRs Point Total (Starting Lineup)" },
+    { id: 6, name: "Highest QB Point Total (Starting Lineup)" },
+    { id: 7, name: "Highest TE Point Total (Starting Lineup)" }
+  ];
 
-  $(".btn-primary").click(function(){
-    var random =  Math.floor(Math.random() * dinner.length);
-    // var chosen = dinner.pop(dinner[random]);
+  $(".btn-primary").click(function() {
+    var random = Math.floor(Math.random() * dinner.length);
     var chosen = dinner[random].name;
-    $('#main').empty().append(chosen);
 
-    // if(dinner.id == 9) {
-    //   $('#main').css('font-size', '50px')
-    // } else {
-    //     $('#main').css('font-size', '100px')
-    // }
+    $("#main")
+      .empty()
+      .append("Randomizing...");
 
+    setTimeout(() => {
+      $("#main")
+        .empty()
+        .append(chosen);
+    }, 4000);
   });
 
-    $('.btn-danger').click(() => {
-        $('#main').empty().append("Stanley's the best. AROUUUND. No ONE's ever gonna take him down!").append("<img src='karatekid.jpeg'>")
-    });
-
+  $(".btn-danger").click(() => {
+    $("#main")
+      .empty()
+      .append(
+        "Stanley's the best. AROUUUND. No ONE's ever gonna take him down!"
+      )
+      .append("<img src='karatekid.jpeg'>");
+  });
 });
